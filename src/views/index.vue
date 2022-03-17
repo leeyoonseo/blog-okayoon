2b46774fcf7d686e761663f246df293a3f3c75f2
leeyoonseo <okayoon.lee@gmail.com>
2022-03-17 23:25:05 +0900

    린트 설정, 페이지 설정, vscode 세팅 설정, 안쓰는 파일 제거 등

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Index</div>
+</template>
+<script lang="ts">
+export default {
+  name: 'indexPage',
+  layout: 'base',
+};
+</script>
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
---
+++ src/views/layouts/base.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><slot></slot></div>
+</template>
+<script lang="ts">
+export default {
+  name: 'BaseLayout',
+};
+</script>
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
