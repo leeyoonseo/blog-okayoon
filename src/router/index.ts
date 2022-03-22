31b4a84b509dc28bcf364f7533852a9128e2c9e1
leeyoonseo <okayoon.lee@gmail.com>
2022-03-22 20:50:45 +0900

    페이지 등록 코드 완성

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import camelCase from 'lodash/camelCase';
 
 const exclude = ['layouts'];
 const views = require.context('@/views/', true, /[a-z]\w+\.vue$/);
@@ -13,25 +14,20 @@ routes = views.keys().reduce((prevRoutes, currentRoutes) => {
     default: { layout, title = 'OKAYOON' },
   } = views(currentRoutes);
   const filePath = filePathSplit.join('/');
-
-  // TODO: name때문에 index가 안나옴
-  // name을 카멜케이스로 변경하자(뎁스가 있는경우)
-  const fileName = filePathSplit[filePathSplit.length - 1].replace('.vue', '');
+  const name = camelCase(filePathSplit.join(' ').replace('.vue', ''));
   const urlPath =
     filePath === 'index.vue'
-      ? `/home`
+      ? `/`
       : `/${filePathSplit
           ?.filter((path) => path !== 'index.vue')
           .join('/')
           .replace('.vue', '')}`;
-  console.log(urlPath);
-
   let route: RouteRecordRaw;
 
   if (layout) {
     route = {
       path: urlPath,
-      name: fileName,
+      name,
       component: () => import(`@/views/layouts/${layout}.vue`),
       meta: {
         title,
@@ -44,10 +40,9 @@ routes = views.keys().reduce((prevRoutes, currentRoutes) => {
       ],
     };
   } else {
-    //
     route = {
       path: urlPath,
-      name: fileName,
+      name,
       component: () => import(`@/views/${filePath}`),
       meta: {
         title,
@@ -56,39 +51,6 @@ routes = views.keys().reduce((prevRoutes, currentRoutes) => {
   }
   return prevRoutes.concat(route);
 }, routes);
-console.log(routes);
-
-// const routes2: Array<RouteRecordRaw> = [
-//   {
-//     path: '/index',
-//     name: 'c',
-//     component: () => import('@/views/layouts/base.vue'),
-//     meta: {
-//       title: 'OKAYOON | HOME',
-//     },
-//     children: [
-//       {
-//         path: '',
-//         component: () => import('@/views/index.vue'),
-//       },
-//     ],
-//   },
-//   {
-//     path: '/about',
-//     name: 'index',
-//     component: () => import('@/views/layouts/base.vue'),
-//     meta: {
-//       title: 'OKAYOON | HOME',
-//     },
-//     children: [
-//       {
-//         path: '',
-//         component: () => import('@/views/about.vue'),
-//       },
-//     ],
-//   },
-// ];
-// console.log(routes2);
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
@@ -96,24 +58,3 @@ const router = createRouter({
 });
 
 export default router;
-
-// {
-//     path: '/a',
-//     name: 'index',
-//     component: BaseLayout,
-//     meta: {
-//       title: 'OKAYOON | HOME',
-//     },
-//     children: [
-//       {
-//         path: 'test',
-//         component: Index,
-//       },
-//     ],
-//   },
-
-// {
-//   path: '/news',
-//   name : 'news',
-//   component: NewsView,
-// },
